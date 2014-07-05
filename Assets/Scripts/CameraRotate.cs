ec3aac73ede88f222835b40806de9f08e56f677d
Arghya Sur <arghyaknight@gmail.com>
2014-07-05 19:49:42 +0530

    first commit

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour
+{
+		public Transform target;
+		// Use this for initialization
+		void Start ()
+		{
+		
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				transform.LookAt (target);
+				transform.Translate (Vector3.right * 40 * Time.deltaTime);
+		}
+}
---
+++ Assets/Scripts/ExitApp.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExitApp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+				Application.Quit(); 
+	}
+}
