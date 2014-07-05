67720875fde36e81b010efd3da9c59a199c56022
Mainak Biswas <mainakbiswas@ymail.com>
2014-07-05 23:51:47 +0530

    moving ball

---
+++ Assets/Scripts/ballDrop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballDrop : MonoBehaviour {
+	public Transform target;
+	private float i = 0;	
+	Vector3 v3 = Vector3.left;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		i = (i + 100 * Time.deltaTime);
+		if (i > 400) {
+			i = 0;
+			v3 = (v3 == Vector3.right) ? Vector3.left : Vector3.right;
+		}
+		transform.Translate(v3 * 40 * Time.deltaTime);
+	}
+}
