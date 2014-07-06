37626986a7732dd23ff297fb0a48b8556a317c92
Arghya Sur <arghyaknight@gmail.com>
2014-07-06 11:55:38 +0530

    cleanups

--- Assets/Scripts/ballDrop.cs
+++ Assets/Scripts/ballDrop.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ballDrop : MonoBehaviour {
-	public Transform target;
-	private float i = 0;	
-	Vector3 v3 = Vector3.left;
-	// Use this for initialization
-	void Start () {
+public class ballDrop : MonoBehaviour
+{
+		public Transform target;
+		private float i = 0;	
+		Vector3 v3 = Vector3.left;
+		// Use this for initialization
+		void Start ()
+		{
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		i = (i + 100 * Time.deltaTime);
-		if (i > 400) {
-			i = 0;
-			v3 = (v3 == Vector3.right) ? Vector3.left : Vector3.right;
 		}
-		transform.Translate(v3 * 40 * Time.deltaTime);
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				i = (i + 100 * Time.deltaTime);
+				if (i > 200) {
+						i = 0;
+						v3 = (v3 == Vector3.right) ? Vector3.left : Vector3.right;
+				}
+				transform.Translate (v3 * 100 * Time.deltaTime);
+		}
 }
